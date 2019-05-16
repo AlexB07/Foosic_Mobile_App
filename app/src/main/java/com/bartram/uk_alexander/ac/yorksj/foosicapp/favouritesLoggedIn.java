d16e1fabb0a3ee4609b45aab9ebd22b38324fb36
Liam <buttonrossi2@sky.com>
2019-05-16 16:46:42 +0100

    app brings to the correct favourites page if logged in or not

--- app/src/main/java/com/bartram/uk_alexander/ac/yorksj/foosicapp/favouritesLoggedIn.java
+++ app/src/main/java/com/bartram/uk_alexander/ac/yorksj/foosicapp/favouritesLoggedIn.java
@@ -2,44 +2,30 @@ package com.bartram.uk_alexander.ac.yorksj.foosicapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class favouritesGuest extends AppCompatActivity {
+public class favouritesLoggedIn extends AppCompatActivity {
     private ImageView imgLogo;
-    private Button btnLogin;
     private SoundbiteNavigationView nav;
     private appdatastorage data = new appdatastorage();
 
     protected void onCreate(Bundle savedInstatnceState){
         super.onCreate(savedInstatnceState);
-
-        if (data.getUserID() != "") {
-            setContentView(R.layout.activity_favourites_loggedin);
-        }
-        else {
+        if (data.getUserID() == "") {
             setContentView(R.layout.activity_favourites_guest);
+        }
+        else{
+            setContentView(R.layout.activity_favourites_loggedin);
             imgLogo = (ImageView) findViewById(R.id.imgLogoTopRight);
-            btnLogin = (Button) findViewById(R.id.btnFavLogin);
             nav = findViewById(R.id.navigation);
 
             imgLogo.setOnClickListener((new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent i = new Intent(v.getContext(), homeScreenGuest.class);
-                    i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                    startActivity(i);
-                }
-            }));
-            btnLogin.setOnClickListener((new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(v.getContext(), LoginScreen.class);
                     i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                     startActivity(i);
                 }
