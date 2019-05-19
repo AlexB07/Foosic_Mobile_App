5c1fb9e42143ac58c29c23a4778d9b1ce2836748
liam.morgan <liam.morgan@yorksj.ac.uk>
2019-05-19 20:30:15 +0100

    Upload New File

---
+++ app/src/main/java/com/bartram/uk_alexander/ac/yorksj/foosicapp/preferences.java
@@ -0,0 +1,17 @@
+package com.bartram.uk_alexander.ac.yorksj.foosicapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class preferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+    public void onStart() {
+        super.onStart();
+    }
+
+}
